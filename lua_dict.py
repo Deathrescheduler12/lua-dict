a5649ba74ae82e5bb0800625c0e14a15bb450d27
Deathrescheduler12 <104992858+Deathrescheduler12@users.noreply.github.com>
2022-06-09 14:39:30 +0300

    Update lua_dict.py

--- lua_dict.py
+++ lua_dict.py
@@ -1,6 +1,5 @@
 class lua_dict:
     def __init__(self, **info):
-        self.parent = []
         for x in info.items():
             if isinstance(x[1], str):
                 exec(f"self.{x[0]} = '{x[1]}'")
