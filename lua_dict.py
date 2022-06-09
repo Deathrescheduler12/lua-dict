968113cddaaa1b5803010fed3a8275fc75c1da15
Deathrescheduler12 <104992858+Deathrescheduler12@users.noreply.github.com>
2022-06-09 15:05:20 +0300

    Update lua_dict.py

--- lua_dict.py
+++ lua_dict.py
@@ -1,6 +1,14 @@
 class lua_dict:
     def __init__(self, **info):
-        for x, y in info.items():
+        self.info = info
+        self.change()
+    def __setitem__(self, name, value):
+        self.info[name] = value
+        self.change()
+    def __getitem__(self, name):
+        return self.info[name]
+    def change(self):
+         for x, y in self.info.items():
             if isinstance(y, str):
                 exec(f"self.{x} = '{y}'")
             else:
